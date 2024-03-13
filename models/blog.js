2fcb8233d66ddec5a93ca26943fc9873dbe11208
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-03-13 20:20:24 +0200

    fix models export/import

--- models/blog.js
+++ models/blog.js
@@ -1,11 +1,23 @@
 const mongoose = require('mongoose')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    minLength: 1
+  },
+  author: {
+    type: String,
+    minLength: 1
+  },
+  url: {
+    type: String,
+    minLength: 1
+  },
+  likes: {
+    type: Number
+  }
 })
+const Blog = mongoose.model('Blog', blogSchema)
 
 blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
@@ -15,4 +27,5 @@ blogSchema.set('toJSON', {
   }
 })
 
-module.exports = mongoose.model('Blog', blogSchema)
+
+module.exports = { Blog }
