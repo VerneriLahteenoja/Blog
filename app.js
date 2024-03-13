c0f93d44c65cbcef89e7dd55a2b1e71adcd41fd8
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-03-13 23:29:31 +0200

    add middleware errorHandler to app use

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use(middleware.errorHandler)
 
 mongoose.connect(config.MONGODB_URI)
 
