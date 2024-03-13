bfc1b45c14394e1b24b9da0cdfb34550fc6db1a2
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-03-13 20:19:44 +0200

    create delete and put functions

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,5 @@
 const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
+const { Blog } = require('../models/blog')
 
 blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({})
@@ -20,6 +20,20 @@ blogsRouter.post('/', async (req, res) => {
   const newBlog = new Blog(blog)
   const savedBlog = await newBlog.save()
   res.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id)
+  res.status(204).end()
+})
+
+blogsRouter.put('/:id', async (req, res) => {
+  const body = req.body
+  const updateData = {
+    url: body.url
+  }
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, updateData, { new: true, runValidators: true })
+  res.status(200).json(updatedBlog)
 })
 
 
