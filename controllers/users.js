445e1074577b524117ce0c349c013b8f85324bd3
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-03-18 20:22:31 +0200

    get users route

--- controllers/users.js
+++ controllers/users.js
@@ -16,4 +16,9 @@ usersRouter.post('/', async (req, res) => {
   res.status(201).json(savedUser)
 })
 
+usersRouter.get('/', async (req, res) => {
+  const users = await User.find({})
+  res.status(200).json(users)
+})
+
 module.exports = usersRouter
