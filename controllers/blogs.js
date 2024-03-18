b8b88c6d30c620557abe6fcab7e26a3b63b08280
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-03-18 20:22:49 +0200

    add status to get res

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,7 +3,7 @@ const { Blog } = require('../models/blog')
 
 blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({})
-  res.json(blogs)
+  res.status(200).json(blogs)
 })
 
 blogsRouter.post('/', async (req, res) => {
