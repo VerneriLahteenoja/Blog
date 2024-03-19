13350b81b3ffc188494059f7b4d7b4c865becc33
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-03-19 19:40:33 +0200

    create basic login router

--- utils/config.js
+++ utils/config.js
@@ -5,8 +5,10 @@ let MONGODB_URI = process.env.NODE_ENV === 'test'
   : process.env.MONGODB_URI
 
 let PORT = process.env.PORT || 3003
+let SECRET = process.env.SECRET
 
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  SECRET
 }
